ccce16f3e801a7653ee72094801402ea9f7dda3c
rimantascode <r.preiks@gmail.com>
2020-10-04 18:16:30 +0000

    added toogle and delete fuctions

--- todo/views.py
+++ todo/views.py
@@ -37,4 +37,16 @@ def edit_item(request, item_id):
     context = {
             'form':form
         }
-    return render(request, 'todo/edit_item.html',context)
+    return render(request, 'todo/edit_item.html', context)
+
+def toggle_item(request, item_id):
+    items = get_object_or_404(item, id=item_id)
+    items.done = not items.done
+    items.save()
+    return redirect('get_todo_list')
+
+
+def delete_item(request, item_id):
+    items = get_object_or_404(item, id=item_id)
+    items.delete()
+    return redirect('get_todo_list')
